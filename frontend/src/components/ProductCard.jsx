25fb02460319393b2fd21bf14c9a98aaa72a6b68
walid <walidkhalid217@gmail.com>
2025-09-24 22:41:43 +0300

    added productcard component with its css

---
+++ frontend/src/components/ProductCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./styles/productcard.css";
+import { Plus, Minus } from "lucide-react";
+
+export default function ProductCard({ product, onAdd, onRemove }) {
+  return (
+    <div className="pcard">
+      <img src={product.image_url} alt={product.name} />
+      <h4>{product.name}</h4>
+      <p>{product.category}</p>
+      <p>KSh {product.price.toFixed(2)}</p>
+      <div className="pcard-btns">
+        {onRemove && <button onClick={() => onRemove(product)}><Minus size={14}/> Remove</button>}
+        {onAdd && <button onClick={() => onAdd(product)}><Plus size={14}/> Add</button>}
+      </div>
+    </div>
+  );
+}
